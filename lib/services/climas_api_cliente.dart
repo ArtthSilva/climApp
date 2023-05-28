53977732d14ea236d2798cd900116bd476089e49
Arthur <113397588+ArtthSilva@users.noreply.github.com>
2023-05-27 21:06:42 -0300

    Update climas_api_cliente.dart

--- lib/services/climas_api_cliente.dart
+++ lib/services/climas_api_cliente.dart
@@ -7,7 +7,7 @@ import 'package:http/http.dart' as http;
 class ClimasApiCliente {
   Future<Clima>? getCurrentClima( ) async {
     final resp =  await http.get(
-      Uri.parse("https://api.openweathermap.org/data/2.5/weather?q=Brazil&appid=eb4c24df3b5f1e328b247cd8913075c5&units=metric"));
+      Uri.parse("API_KEY"));
 
       if (resp.statusCode == 200) {
         Map<String, dynamic> json = jsonDecode(resp.body);
